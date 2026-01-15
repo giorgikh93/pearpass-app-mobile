ec1db1dc2ff202ba8fcb4c1e1b34b3ff422bba1f
Nikoloz Dolidze <n.dolidze@noxtton.com>
2026-01-15 16:14:42 +0400

    Update pairing texts (#80)
    * Update pairing texts
    
    * Minor refactor
    
    * Minor refactor

--- src/containers/BottomSheetAddDeviceContent/index.jsx
+++ src/containers/BottomSheetAddDeviceContent/index.jsx
@@ -49,8 +49,8 @@ export const BottomSheetAddDeviceContent = () => {
 
       <Text style={styles.description}>
         {activeTab === TAB.PAIR_THIS
-          ? t`Scan this QR code or paste the vault key into the PearPass app on your other device to connect it to your vault. This method keeps your vault secure.`
-          : t`Scan the QR code or paste the vault key from the PearPass app on your other device to connect it to your vault. This method keeps your vault secure.`}
+          ? t`Scan this QR code or paste the vault key into the PearPass app on your other device to connect it to your account. This method keeps your account secure.`
+          : t`Scan the QR code or paste the vault key from the PearPass app on your other device to connect it to your account. This method keeps your account secure.`}
       </Text>
 
       {activeTab === TAB.PAIR_THIS ? (
@@ -71,7 +71,7 @@ export const BottomSheetAddDeviceContent = () => {
                 activeOpacity={0.7}
                 onPress={() => setActiveTab(TAB.PAIR_ANOTHER)}
               >
-                <Text style={styles.tabText}>{t`Load a Vault`}</Text>
+                <Text style={styles.tabText}>{t`Import vault`}</Text>
               </TouchableOpacity>
             </View>
           }
@@ -95,7 +95,7 @@ export const BottomSheetAddDeviceContent = () => {
                 onPress={() => setActiveTab(TAB.PAIR_ANOTHER)}
               >
                 <Text style={[styles.tabText, styles.tabTextActive]}>
-                  {t`Load a Vault`}
+                  {t`Import vault`}
                 </Text>
               </TouchableOpacity>
             </View>
